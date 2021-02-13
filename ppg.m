35a0ef8e4caaa01a886e25ed617a6ccf4c729ea4
Katelyn Li <65512164+katelynsli@users.noreply.github.com>
2021-02-13 14:52:00 -0500

    Add files via upload

---
+++ ppg.m
@@ -0,0 +1,16 @@
+% accepts string with video file name and returns framerate variable
+function framerate = ppg(vid)
+
+[subj, framerate] = readVideo(vid);
+
+phone_ppg{1} = videoToPPG(subj{1}, framerate);
+phone_ppg{2} = videoToPPG(subj{2}, framerate);
+phone_ppg{3} = videoToPPG(subj{3}, framerate);
+
+% plot ppg channels on different figures
+close all
+plot(phone_ppg{1})
+figure
+plot(phone_ppg{2})
+figure
+plot(phone_ppg{3})
