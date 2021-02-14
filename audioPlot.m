33e77b06fb336d1c696c3c76144abcdc9d23d2ad
le-rain <kwak.lorianne@gmail.com>
2021-02-14 02:42:00 -0500

    created function to read audio data and plot data to find peaks

---
+++ audioPlot.m
@@ -0,0 +1,15 @@
+% This plots the audio files and finds their peaks
+close all;
+
+figure(1)
+audiopeaks('Audio/lorianne-iphone7.m4a');
+
+figure(2)
+audiopeaks('Audio/lorianne-iphone7-2.m4a');
+
+figure(3)
+audiopeaks('Audio/lorianne-iphone11.m4a');
+
+figure(4)
+audiopeaks('Audio/lorianne-iphone11-2.m4a');
+
