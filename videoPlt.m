aaadc05af47e30ab352b4669effb21e57a88a26c
kristinchin <chin.k@husky.neu.edu>
2021-02-13 21:24:33 -0500

    uploaded videos to github

--- videoPlt.m
+++ videoPlt.m
@@ -1,15 +1,15 @@
+close all;
 % plots videos 
-% figure(1)
-% ppg('Video/katelyn-1.mov');
+ figure(1)
+ ppg('Video/katelyn-1.mov');
 % figure(2)
 % ppg('Video/katelyn-heightchanges-11pro-1.mov');
-close all;
-figure(3)
-ppg('Video/katelyn-heightchanges-5-1.mov');
-figure(4)
-ppg('Video/lorianne-1.mp4');
-% figure(5)
-% ppg('Video/kristin-7-back-flash-1.MOV');
+% figure(3)
+% ppg('Video/katelyn-heightchanges-5-1.mov');
+% figure(4)
+% ppg('Video/lorianne-1.mp4');
+figure(5)
+ppg('Video/kristin-7-back-flash-1.MOV');
 % figure(6)
 % ppg('Video/kristin-7-back-noflash-1.mov');
 % figure(7)
