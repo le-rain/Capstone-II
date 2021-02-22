ab6cdb7030baeb69bb4534a8505fac95841993a2
gitgudgoldfish <78772267+gitgudgoldfish@users.noreply.github.com>
2021-02-22 17:38:34 -0500

    Add files via upload

--- PTT.m
+++ PTT.m
@@ -4,7 +4,8 @@ ecg009 = signal.ecg.y;
 [pksppg, locsppg] = findpeaks(ppg009, 'MinPeakDistance', 140);
 [pksecg, locsecg] = findpeaks(ecg009, 'MinPeakDistance', 140);
 
-tm = [0:1/300:480];
+record_time = length(ppg009);
+tm = [1:record_time];
 tm = tm';
 
 figure (1)
@@ -22,7 +23,7 @@ if (peaks_count_ppg < peaks_count_ecg)
     while (n < peaks_count_ppg)
         if (locsppg(n) - locsecg(n) <140)
             peak_pair_ppg(n) = locsppg(n);
-            peal_pair_ecg(n) = locsecg(n);
+            peak_pair_ecg(n) = locsecg(n);
         n = n+1;
         end
     end
@@ -31,13 +32,13 @@ else
     while (n < peaks_count_ecg)
         if (locsppg(n) - locsecg(n) < 140)
             peak_pair_ppg(n) = locsppg(n);
-            peal_pair_ecg(n) = locsecg(n);
+            peak_pair_ecg(n) = locsecg(n);
         n = n+1;
         end
     end
 end
 
-delay = peaks_pair_ppg - peaks_pair_ecg;
+delay = peak_pair_ppg - peak_pair_ecg;
 average_delay = mean(delay);
 PTT = average_delay *(1000/300)
 
