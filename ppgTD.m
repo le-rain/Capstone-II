e80950b29f5275f36d4730d5c9cf32d4ddb150f7
Katelyn Li <katelyn_li@yahoo.com>
2021-03-19 12:19:56 -0400

    Update ppgTD.m

--- ppgTD.m
+++ ppgTD.m
@@ -1,15 +1,18 @@
-function ppg_td = ppgTD(file, roiHeight, roiWidth)
-
-    [video, height, width, frameRate] = readVideoTD(file);
-    [redTD, greenTD] = mapTD(video, height, width, frameRate);
-    [red, green, roiHeight, roiWidth] = splitVideoTD(redTD, greenTD, height, width, roiHeight, roiWidth);
-    [roi,i,j] = selectRegionTD(red, green);
-    ppg = videoToPPGTD(video,roi,i,j,roiHeight,roiWidth,frameRate);
+%accepts video file and desired ROI height and width in pixels
+%returns PPG, time vector, frame rate, ROI channel, and ROI indices with
+%respect to original video frame
+%roi = 1 for red channel and roi = 2 for green channel
+function [ppg_td, time, frameRate, roi, i, j] = ppgTD(file, roiHeight, roiWidth)
+    [video, height, width, frameRate] = readVideoTD(file); %read video
+    [redTD, greenTD] = mapTD(video, height, width, frameRate); %calculate temporal differences
+    [red, green, roiHeight, roiWidth] = splitVideoTD(redTD, greenTD, height, width, roiHeight, roiWidth); %split TD arrays into ROI blocks
+    [roi,i,j] = selectRegionTD(red, green); %select best ROI
+    ppg_td = videoToPPGTD(video,roi,i,j,roiHeight,roiWidth,frameRate); %plot ROI over duration of video
     
     %plot PPG
-    time = [1:length(ppg)] ./ frameRate; %generate time vector
+    time = [1:length(ppg_td)] ./ frameRate; %generate time vector
     figure %create new figure
-    plot(time,ppg)
+    plot(time,ppg_td)
     title(file) 
     xlabel('Time (s)')
     ylabel('PPG (a.u.)')
