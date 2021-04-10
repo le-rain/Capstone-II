afd6f209396007821f5047fb86db47b0589013fd
Katelyn Li <katelyn_li@yahoo.com>
2021-04-10 17:36:29 -0400

    Update calculatePTT.m

--- calculatePTT.m
+++ calculatePTT.m
@@ -1,9 +1,9 @@
 %integrates ppgTM and audioPlot to calculate PTT from best sections of both
 %signals
-function section_overlap = calculatePTT(ppg_sections,audio_sections)
+function section_overlap = calculatePTT(ppg_sections,audio_sections,ppg_tm)
     section_overlap = zeros(1,2);
     k = 1;
-    %find overlap between ppg_sections and audio_sections
+    %find indices of overlap(s) between ppg_sections and audio_sections
     for i = 1:length(ppg_sections)
         for j = 1:length(audio_sections)
             if ppg_sections(i,2) > audio_sections(j,1) && ppg_sections(i,1) < audio_sections(j,1) && ppg_sections(i,2) <= audio_sections(j,2) 
@@ -33,7 +33,16 @@ function section_overlap = calculatePTT(ppg_sections,audio_sections)
             end
         end
     end
-    %find locations of ppg peaks
-    %find locations of audio peaks
-    %calculate ptt
+    
+    [ppg_pks,ppg_locs] = cell(1,2);
+    [audio_pks,audio_locs] = cell(1,2);
+    for i = 1:length(section_overlap)
+        %find locations of ppg peaks
+        [ppg_pks{i},ppg_locs{i}] = findpeaks(ppg_tm(section_overlap(i,1):ppg_tm(section_overlap(i,2),'MinPeakDistance',15); 
+        %find locations of audio peaks
+        %[audio_pks{i},audio_locs{i}] = 
+        %calculate ptt
+        ptt = avg(ppg_locs{i} - audio_locs{i});
+    end
+    
 end
