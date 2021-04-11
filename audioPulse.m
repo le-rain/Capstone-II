e25ae209ce110d728aa9f3f31626a29babb977f3
le-rain <kwak.lorianne@gmail.com>
2021-04-11 14:19:05 -0400

    updated audioPulse to return sampling frequency and audio sections in time and sample number

--- audioPulse.m
+++ audioPulse.m
@@ -1,5 +1,15 @@
-% Call this function to find sections in audio with good consecutive peaks
-function [sectionI] = audioPulse(audio, bpm)
+% Call this function to find sections in audio with good consecutive peaks%
+
+% Inputs:
+% audio is name of audio files in this format: 'File/filename.ext'
+% bpm is estimated BPM
+
+% Outputs:
+% sectionT is sections of audio in seconds
+% sectionI is sections of audio in sample#
+% Fs is sampling frequency
+
+function [sectionT, sectionI, Fs] = audioPulse(audio, bpm)
 
 % Read audio from video and find peaks
 [audio, peaks, time, x, y, Fs, minDist] = findAudioPeaks(audio, bpm);
