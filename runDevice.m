0d41756a894276450ca02553c6a383b2b7418966
Katelyn Li <katelyn_li@yahoo.com>
2021-04-19 23:04:47 -0400

    Modified calculatePTT and runDevice

--- runDevice.m
+++ runDevice.m
@@ -1,5 +1,5 @@
-function [ppg_tm,audio_data,frameRate,time,ppg_sections,audio_sections,section_overlap,ppg_locs,audio_locs,all_S2] = runDevice(file)
+function [ppg_tm,audio_data,frameRate,time,ppg_sections,audio_sections,section_overlap,ppg_locs,audio_locs,all_S2,ptt_calc,ppg_amp] = runDevice(file)
     [ppg_tm,~,~,time,frameRate,~,~,~,~,~,~,ppg_sections] = ppgTM(file);
-    [sectionI, ~, allS2, Fs, audioData] = audioPulse(file, 1, 0);
-    [audio_sections,audio_data,section_overlap,ppg_locs,audio_locs,all_S2,~] = calculatePTT(ppg_tm,audioData,ppg_sections,sectionI,allS2,frameRate,Fs);
+    [sectionI, ~, allS2, Fs, audioData] = audioPulse(file, 0, 0, 1);
+    [audio_sections,audio_data,section_overlap,ppg_locs,audio_locs,all_S2,ptt_calc,ppg_amp] = calculatePTT(ppg_tm,audioData,ppg_sections,sectionI,allS2,frameRate,Fs)
 end
